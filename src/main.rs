cefbc7efb0690c61e60e58e4dcd6793d4727ff66
JQiue <861947542@qq.com>
2024-06-12 12:31:02 +0800

    fix: use proper time zone for cron job

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod hifini;
 
 use std::{env, error::Error};
-
 use tokio_cron_scheduler::{Job, JobScheduler};
 
 #[tokio::main]
@@ -12,12 +11,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 async fn start() {
-  let schedule = env::var("SCHEDULE_RULE").unwrap_or("0 0 8 * * * *".to_string());
+  let schedule = env::var("SCHEDULE_RULE").unwrap_or("10 24 12 * * * *".to_string());
   let sched = JobScheduler::new().await.expect("Canot create JobScheduler");
-  let job = Job::new(schedule.as_str(), |_uuid, _l| {
+  let job = Job::new_tz(schedule.as_str(), chrono_tz::Asia::Shanghai, |_, _| {
+    println!(">>> HiFiNi");
     tokio::spawn(hifini::sign_in());
   })
-  .expect("Canot create a new cron job");
+  .expect("Canot create a new job");
   sched.add(job).await.expect("Canot add a job to the JobScheduler");
   sched.start().await.expect("Cannot run all jobs");
   tokio::signal::ctrl_c().await.unwrap();
